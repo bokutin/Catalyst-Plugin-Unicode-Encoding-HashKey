9ea1f522de1adaac15b62f1f471bdb9900dd922b
Tomohiro Hosaka <bokutin@bokut.in>
2013-06-27 05:46:31 +0900

    ハッシュキーにNFCの標準化を追加。

--- lib/Catalyst/Plugin/Unicode/Encoding/HashKey.pm
+++ lib/Catalyst/Plugin/Unicode/Encoding/HashKey.pm
@@ -3,6 +3,20 @@ package Catalyst::Plugin::Unicode::Encoding::HashKey;
 
 use strict;
 use base qw(Catalyst::Plugin::Unicode::Encoding);
+use Unicode::Normalize qw(NFC);
+
+__PACKAGE__->mk_classdata('_normalize_hash_key_code');
+
+sub normalize_hash_key_code {
+    my $c = shift;
+
+    $c->_normalize_hash_key_code or $c->_normalize_hash_key_code(
+        do {
+            my $flag = $c->config->{'Plugin::Unicode::Encoding::HashKey'}{normalize_hash_key_nfc} // 0;
+            $flag ? sub { NFC($_[0]) } : sub { $_[0] };
+        }
+    );
+}
 
 sub prepare_uploads {
     my $c = shift;
@@ -12,25 +26,28 @@ sub prepare_uploads {
     my $enc = $c->encoding;
     return unless $enc;
 
+    my $n = $c->normalize_hash_key_code;
+
     for my $key (qw(parameters query_parameters body_parameters)) {
         for (keys %{ $c->req->{$key} }) {
-            $c->req->{$key}{ $c->_handle_unicode_decoding($_) } = delete $c->req->{$key}{$_};
+            $c->req->{$key}{ $n->($c->_handle_unicode_decoding($_)) } = delete $c->req->{$key}{$_};
         }
     }
 }
 
-sub _handle_unicode_decoding {
-    my $c = shift;
-    my ($value) = @_;
-
-    if (defined $value and ref $value eq 'HASH') {
-        for (keys %$value) {
-            $value->{ $c->_handle_unicode_decoding($_) } = delete $value->{$_};
-        }
-    }
-
-    $c->next::method(@_);
-}
+#sub _handle_unicode_decoding {
+#    my $c = shift;
+#    my ($value) = @_;
+#
+#    if (defined $value and ref $value eq 'HASH') {
+#        my $n = $c->normalize_hash_key_code;
+#        for (keys %$value) {
+#            $value->{ $n->($c->_handle_unicode_decoding($_)) } = delete $value->{$_};
+#        }
+#    }
+#
+#    $c->next::method(@_);
+#}
 
 1;
 
@@ -51,6 +68,12 @@ Catalyst::Plugin::Unicode::Encoding::HashKey - params のキーもデコード�
     extends qw(Catalyst);
 
     override _default_plugins => sub { map { $_ eq "Unicode::Encoding" ? $_."::HashKey" : () } super };
+
+    __PACKAGE__->config(
+        'Plugin::Unicode::Encoding::HashKey' => {
+            normalize_hash_key_nfc => 1, # default
+        },
+    );
 
     __PACKAGE__->setup(
         ...
--- t/basic.t
+++ t/basic.t
@@ -36,4 +36,40 @@ sub Dump { require YAML::Syck; goto &YAML::Syck::Dump }
     ] } ) or diag Dump $c->req->params;
 }
 
+# normalize
+use Unicode::Normalize qw(NFC NFD);
+{
+    my $uri = URI->new("/");
+    $uri->query_form(
+        NFC("がぎぐげご") => NFC("ざじずぜぞ"),
+        NFD("だぢづでど") => NFD("ぱぴぷぺぽ"),
+    );
+
+    {
+        t::lib::TestApp->_normalize_hash_key_code(undef);
+        t::lib::TestApp->config->{'Plugin::Unicode::Encoding::HashKey'}{normalize_hash_key_nfc} = 0;
+        my ($res, $c) = ctx_request($uri->as_string);
+
+        my @keys   = sort keys $c->req->params;
+        my @values = sort values $c->req->params;
+        is( length $keys[0]   ,  5 );
+        is( length $keys[1]   , 10 );
+        is( length $values[0] ,  5 );
+        is( length $values[1] , 10 );
+    }
+
+    {
+        t::lib::TestApp->_normalize_hash_key_code(undef);
+        t::lib::TestApp->config->{'Plugin::Unicode::Encoding::HashKey'}{normalize_hash_key_nfc} = 1;
+        my ($res, $c) = ctx_request($uri->as_string);
+
+        my @keys   = sort keys $c->req->params;
+        my @values = sort values $c->req->params;
+        is( length $keys[0]   ,  5 );
+        is( length $keys[1]   ,  5 );
+        is( length $values[0] ,  5 );
+        is( length $values[1] , 10 );
+    }
+}
+
 done_testing();
